7f3fd2931d7fafd3dfe900ed780fa2fc4d08a7dd
leekrheon <juheon1155@naver.com>
2026-06-15 15:18:40 +0900

    Update App.tsx

--- IMBY/src/app/App.tsx
+++ IMBY/src/app/App.tsx
@@ -2893,7 +2893,7 @@ export default function App() {
             display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '0 4vw',
             opacity: 0, pointerEvents: 'none' }}>
             <img ref={typo3Ref} src="/ment3.svg" alt="ment3"
-              style={{ width: '105%', objectFit: 'contain', objectPosition: 'left center', filter: 'invert(1)' }} />
+              style={{ width: '80%', objectFit: 'contain', objectPosition: 'left center', filter: 'invert(1)' }} />
           </div>
         </div>
 
